6da22358757063ab01b31f4e85dfe198d053111c
MykhailoPalonnyi <hlebushek156@gmail.com>
2024-10-30 13:27:04 +0000

    initial player controller

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]private float Speed = 5f;
+    [SerializeField]private float JumpForce = 10f;
+
+    private Rigidbody2D rb;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask Ground;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * Speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.1f, Ground);
     }
 }
