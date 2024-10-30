660069e5815b727fb1d1458551444216f3bbdf55
MykhailoPalonnyi <hlebushek156@gmail.com>
2024-10-30 18:04:05 +0000

    assigned initial animations

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -8,17 +8,25 @@ public class playerController : MonoBehaviour
     [SerializeField]private float JumpForce = 10f;
 
     private Rigidbody2D rb;
+    private Animator anim;
+
+    
 
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask Ground;
+    
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        AnimationController();
+
         float moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * Speed, rb.velocity.y);
 
@@ -35,5 +43,13 @@ public class playerController : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.1f, Ground);
+    }
+    private void AnimationController()
+    {
+        bool isMoving = rb.velocity.x != 0;
+        anim.SetBool("isMoving", isMoving);
+
+        anim.SetFloat("ySpeed", rb.velocity.y);
+        anim.SetBool("isGrounded", IsGrounded());
     }
 }
